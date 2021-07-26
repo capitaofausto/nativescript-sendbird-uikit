688c9f08bca3240265aab50dde655775c6eab4f8
bentovsky <jpmbento95@gmail.com>
2021-07-26 18:20:12 +0100

    feat(android): add basic function to create, enter and send messages to a channel

--- packages/sendbird/index.d.ts
+++ packages/sendbird/index.d.ts
@@ -1,5 +1,9 @@
 import { SendbirdCommon } from './common';
 
 export declare class Sendbird extends SendbirdCommon {
-    
+  init(): void {}
+  connect(userId: string): Promise<{data: com.sendbird.android.User}> {}
+  createChannel(): Promise<{data: string}> {}
+  enterChannel(channelId: string): Promise<{data: string} | {error: any}> {}
+  sendMessage(message: string): Promise<{data: string} | {error: any}> {}
 }

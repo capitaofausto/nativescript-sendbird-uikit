8d2a367099e7aa7cc8306c862b2d68e0f6f852e2
bentovsky <jpmbento95@gmail.com>
2021-07-26 12:55:09 +0100

    feat(android): add sendbird sdk typings to platforms directory

--- apps/demo-angular/src/app.component.ts
+++ apps/demo-angular/src/app.component.ts
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
 		<page-router-outlet></page-router-outlet>
 	</GridLayout>`,
 })
-export class AppComponent {}
+export class AppComponent {
+}
---
+++ packages/sendbird/angular/index.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
+import { Sendbird } from '@nativescript/sendbird';
+
+@NgModule()
+export class NativeScriptSendbirdModule {}
+
+// Uncomment this line if the package provides a custom view component
+// registerElement('Sendbird', () => Sendbird);
